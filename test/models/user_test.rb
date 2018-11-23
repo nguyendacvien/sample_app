a384d8bc0288bd9878140c4498e7295879827a6d
kyn <nguyendacvien@gmail.com>
2018-11-23 16:08:47 +0700

    Final lan 2
    Final
    
    Add a Static Pages controllers
    
    Finish staticpages
    
    Complete advanced setup
    
    Add a full_title helper
    
    Finish layout and routes
    
    Fix rubocop
    
    create users modeling
    
    add users model
    
    fix
    
    fix 2
    
    add
    
    Final
    
    Final
    
    add
    
    Fix rubocop chapter 8 user.rb
    
    Fix

---
+++ test/models/user_test.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  before_save{self.email = email.downcase}
+  validates :name,  presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255},
+                    format: {with: VALID_EMAIL_REGEX},
+                    uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 6}
+
+  # Returns the hash digest of the given string.
+  def self.digest string
+    if (cost = ActiveModel::SecurePassword.min_cost)
+      BCrypt::Engine::MIN_COST
+    else BCrypt::Engine.cost
+    end
+    BCrypt::Password.create(string, cost: cost)
+  end
+end
